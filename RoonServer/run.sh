9b624024ad88677a49b15a72a6ec0567d047df5f
Matthijs <28700038+matthijsberg@users.noreply.github.com>
2021-04-22 14:30:43 +0200

    Update run.sh
    Added a default back-up option to config for a last resort help. Using native back-up. trust user with container back-ups.

--- RoonServer/run.sh
+++ RoonServer/run.sh
@@ -2,7 +2,7 @@
 cd /backup
 if test ! -d roon; then
         echo "Back-up directory not found, creating in /backup/roon for roon native back-ups. Mapped to HA Back-up dir."
-        mkdir /roon
+        mkdir roon
 fi
 mkdir /app
 cd /app
@@ -12,5 +12,18 @@ if test ! -d RoonServer; then
         tar xjf $ROON_SERVER_PKG
         rm -f $ROON_SERVER_PKG
 fi
-echo Roonserver Installed, starting service.
+echo "Roonserver Installed"
+if test ! -f backups; then
+        echo "Back-up schedule not found, downloading default for this HA install"
+        wget https://github.com/matthijsberg/HomeAssistant-Addons/blob/main/RoonServer/config/backups
+else
+        echo "Existing Backup schedule config found, not toutching. DO CHECK FILE PATH! Should point to /backup. That folder is mapped to HA backup folder."
+fi
+if test ! -f backups_status; then
+        echo "Default back-up path not set, downloading default for this HA install"
+        wget https://github.com/matthijsberg/HomeAssistant-Addons/blob/main/RoonServer/config/backups_status
+else
+        echo "Default back-up path config file found, not toutching. DO CHECK FILE PATH! Should point to /backup. That folder is mapped to HA backup folder."
+fi
+echo "Installation an basic configuration finished! Starting service."
 /app/RoonServer/start.sh
