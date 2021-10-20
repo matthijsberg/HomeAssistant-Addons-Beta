7bd64bdd29636bf0230831e7535c32d661c6c6e5
Matthijs <28700038+matthijsberg@users.noreply.github.com>
2021-10-20 14:47:18 +0200

    Update run.sh
    Fix for new installs where path for backup was not there yet.

--- RoonServer/run.sh
+++ RoonServer/run.sh
@@ -13,21 +13,31 @@ if test ! -d RoonServer; then
         rm -f $ROON_SERVER_PKG
 fi
 echo "Roonserver Installed"
-sleep 4h
+sleep 1s
+if test ! -d /data/RoonServer/Database/Registry/Core; then
+        echo "First time Roon install, no existing directories for roon found in data partition"
+        echo "Creating directories manually to setup automated back-up schedule..."
+        mkdir /data/RoonServer/
+        mkdir /data/RoonServer/Database/
+        mkdir /data/RoonServer/Database/Registry/
+        mkdir /data/RoonServer/Database/Registry/Core
+        echo "   created /data/RoonServer/Database/Registry/Core"
+else
+        echo "Existing installation found, checking config files for backup..."
+fi
 cd /data/RoonServer/Database/Registry/Core
 if test ! -f backups; then
-        echo "Back-up schedule not found, downloading default for this HA install"
+        echo "   Back-up schedule not found, downloading default for this HA install"
         wget https://github.com/matthijsberg/HomeAssistant-Addons/blob/main/RoonServer/config/backups
 else
-        echo "Existing Backup schedule config found, not toutching. DO CHECK FILE PATH! Should point to /backup. That folder is mapped to HA backup folder."
+        echo "   Existing Backup schedule config found, not toutching. DO CHECK FILE PATH! Should point to /backup. That folder is mapped to HA backup folder."
 fi
 if test ! -f backups_status; then
-        echo "Default back-up path not set, downloading default for this HA install"
+        echo "   Default back-up path not set, downloading default for this HA install"
         wget https://github.com/matthijsberg/HomeAssistant-Addons/blob/main/RoonServer/config/backups_status
 else
-        echo "Default back-up path config file found, not toutching. DO CHECK FILE PATH! Should point to /backup. That folder is mapped to HA backup folder."
+        echo "   Default back-up path config file found, not toutching. DO CHECK FILE PATH! Should point to /backup. That folder is mapped to HA backup folder."
 fi
 echo "Installation an basic configuration finished! Starting service."
 cd /app/RoonServer/
-pause
 /app/RoonServer/start.sh
