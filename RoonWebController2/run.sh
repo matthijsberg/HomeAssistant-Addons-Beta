a69b528f3007cb215a36596b1c39778a2f12d8d8
Matthijs <28700038+matthijsberg@users.noreply.github.com>
2021-10-20 17:41:39 +0200

    Update run.sh

--- RoonWebController2/run.sh
+++ RoonWebController2/run.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 set -e
 
-## temp
-sleep 4h
+## Download and install
+git clone https://github.com/pluggemi/roon-web-controller.git
+cd roon-web-controller/
+#git checkout alpha
+npm run build
+npm audit fix #--force
+npm start
 
 ## Run Program
-node /roon-web-controller/app.js
+#node /roon-web-controller/app.js
 #echo Hello world!
