016fe3ce5f3d9be0067d84c31d387dd7749a3c08
Matthijs <28700038+matthijsberg@users.noreply.github.com>
2022-03-30 12:43:42 +0200

    Update run.sh
    changes curl to wget to download (wget honors 301 redirects, curl does not. Change in download broke this)

--- RoonServer/run.sh
+++ RoonServer/run.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/with-contenv bashio
-
-sleep 5000s
-
 cd /backup
 if test ! -d roon; then
         echo "Back-up directory not found, creating in /backup/roon for roon native back-ups. Mapped to HA Back-up dir."
@@ -11,7 +8,7 @@ mkdir /app
 cd /app
 if test ! -d RoonServer; then
         echo "Downloading Roon Server Installer"
-        curl $ROON_SERVER_URL -O
+        wget $ROON_SERVER_URL
         tar xjf $ROON_SERVER_PKG
         rm -f $ROON_SERVER_PKG
 fi
